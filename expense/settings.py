3e1e327cf9edd5632b3424f794d64f6fef595612
Nebula <nebulaanish@gmail.com>
2023-11-22 20:55:12 +0545

    Feat : Login, Register, Create designs created

--- expense/settings.py
+++ expense/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'crispy_forms',
+    'crispy_tailwind',
     'base',
 ]
 
@@ -122,6 +125,11 @@ LOGIN_URL = 'login'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
+CRISPY_TEMPLATE_PACK = "tailwind"
+    
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
